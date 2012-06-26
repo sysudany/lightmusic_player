049663dbb1085077487f689e646094ab74fb05af
sysudany <chdany@qq.com>
2012-06-26 19:43:56 +0800

    626 19:23

--- src/dany/player/PlayerActivity.java
+++ src/dany/player/PlayerActivity.java
@@ -11,27 +11,39 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import dany.player.PlayerService.PlayerBinder;
+import dany.player.view.WmtRatingBar;
+import dany.player.view.WmtRatingBar.OnRatingBarChanging;
 
 public class PlayerActivity extends Activity implements OnClickListener {
 
 	public static final String UPDATE_UI = "dany.player.UPDATE_UI";
 	PlayerService mService;
-	BroadcastReceiver updateReceiver;
 	boolean mBound = false;
+	boolean mRegistered ;
+	BroadcastReceiver updateReceiver;
+
 
 	private Button bt_control;
 	private TextView tv_title;
-	private TextView tv_artist;
-
 	Animation alphaAnimation;
+	private WmtRatingBar mVoluemRatingBar;
+	private LinearLayout ll_vol;
+	private TextView test;
+
+	private Handler handler = new Handler();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -41,31 +53,43 @@ public class PlayerActivity extends Activity implements OnClickListener {
 		bt_control = (Button) this.findViewById(R.id.bt_control);
 		bt_control.setOnClickListener(this);
 		alphaAnimation = AnimationUtils.loadAnimation(this, R.anim.alpha);
-
 		tv_title = (TextView) this.findViewById(R.id.tv_title);
-		tv_artist = (TextView) this.findViewById(R.id.tv_artist);
-
+		ll_vol = (LinearLayout) this.findViewById(R.id.ll_vol);
+		
+		
+		
+		test = (TextView) findViewById(R.id.test);
+		mVoluemRatingBar = (WmtRatingBar) findViewById(R.id.volume_ratingBar);
+		mVoluemRatingBar.setOnRatingBarChange(new OnRatingBarChanging() {
+			@Override
+			public void onRatingChanging(float f) {
+				test.setText("Volume=" + f);
+			}
+		});
 	}
 
 	protected void onStart() {
 		super.onStart();
 		Intent intent = new Intent(this, PlayerService.class);
-		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+		getApplicationContext().bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
 
 		updateReceiver = new UpdateReceiver();
 		IntentFilter filter = new IntentFilter();
 		filter.addAction(UPDATE_UI);
 		registerReceiver(updateReceiver, filter);
+		mRegistered = true;
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		if (mBound) {
-			unbindService(mConnection);
+			getApplicationContext().unbindService(mConnection);
 			mBound = false;
 		}
-		unregisterReceiver(updateReceiver);
+		if(mRegistered)
+			unregisterReceiver(updateReceiver);
+		mRegistered = false;
 	}
 
 	private class UpdateReceiver extends BroadcastReceiver {
@@ -73,9 +97,8 @@ public class PlayerActivity extends Activity implements OnClickListener {
 		public void onReceive(Context context, Intent intent) {
 			if (intent.getBooleanExtra(PlayerService.ERROR, false)) {
 				Toast.makeText(PlayerActivity.this, "没有列表", 1).show();
-				unbindService(mConnection);
-			}else{
-				tv_artist.setText(mService.getArtist());
+				getApplicationContext().unbindService(mConnection);
+			} else {
 				tv_title.setText(mService.getMusicName());
 			}
 		}
@@ -94,40 +117,95 @@ public class PlayerActivity extends Activity implements OnClickListener {
 			mBound = false;
 		}
 	};
+
+	//创建菜单
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.menu, menu);
+	    return true;
+	}
 	
-	private boolean isPlaying ;
+	//菜单的点击事件
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.settings:
+	            
+	            return true;
+	        case R.id.exit:
+	            onDestroy();
+	            finish();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	private boolean isPlaying;
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.bt_control:
-			if(isPlaying)
-			{
+			if (isPlaying) {
 				mService.pause();
 				v.setBackgroundResource(R.drawable.play_selector);
 				v.setAnimation(alphaAnimation);
-			}else{
+				isPlaying = false;
+			} else {
 				mService.start();
 				v.setBackgroundResource(R.drawable.pause_selector);
 				v.setAnimation(alphaAnimation);
+				isPlaying = true;
 			}
 			break;
 		default:
 			break;
 		}
 	}
-	
+
+	//复写返回键
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if(keyCode==KeyEvent.KEYCODE_BACK){
-			 Intent mHomeIntent = new Intent(Intent.ACTION_MAIN);
-
-             mHomeIntent.addCategory(Intent.CATEGORY_HOME);
-             mHomeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                             | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-             this.startActivity(mHomeIntent);
-             return true;
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			Intent mHomeIntent = new Intent(Intent.ACTION_MAIN);
+			mHomeIntent.addCategory(Intent.CATEGORY_HOME);
+			mHomeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+			this.startActivity(mHomeIntent);
+			overridePendingTransition(R.anim.scale_in, R.anim.scale_out);
+			return true;
 		}
 		return super.onKeyDown(keyCode, event);
+	}
+	
+	//实现音量控制板的显示和消失
+	private long timeWhenYouTouch = 0;
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			if (ll_vol.getVisibility() == View.INVISIBLE) {
+				if (timeWhenYouTouch != 0) {
+					long interval = System.currentTimeMillis() - timeWhenYouTouch;
+					if (interval < 1000) {
+						ll_vol.setVisibility(View.VISIBLE);
+						handler.postDelayed(new Runnable() {
+							@Override
+							public void run() {
+								ll_vol.setVisibility(View.INVISIBLE);
+								timeWhenYouTouch = 0;
+							}
+						}, 5000);
+					}
+				}
+				timeWhenYouTouch = System.currentTimeMillis();
+			}
+			break;
+		default:
+			break;
+		}
+		return super.onTouchEvent(event);
 	}
 
 }
--- src/dany/player/PlayerService.java
+++ src/dany/player/PlayerService.java
@@ -3,33 +3,82 @@ package dany.player;
 import java.io.File;
 import java.util.List;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Binder;
 import android.os.Environment;
 import android.os.IBinder;
+import android.telephony.TelephonyManager;
 import dany.player.bean.Music;
 import dany.player.util.FileUtil;
 
 public class PlayerService extends Service {
 
 	public static final String ERROR = "getList";
-	
+
 	private final IBinder mBinder = new PlayerBinder();
-	
+
 	private MediaPlayer mPlayer;
 	private List<Music> musicList;
 	private int musicIndex;
 
+	private PhoneComingReceiver receiver;
+
+	private NotificationManager notificationManager;
+	private Notification notification;
+	
+	private class PhoneComingReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
+				pause();
+			} else {
+				// 如果是来电
+				TelephonyManager tm = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
+				switch (tm.getCallState()) {
+				case TelephonyManager.CALL_STATE_RINGING:
+					pause();
+					break;
+				case TelephonyManager.CALL_STATE_OFFHOOK:
+					pause();
+					break;
+				case TelephonyManager.CALL_STATE_IDLE:
+					start();
+					break;
+				}
+			}
+		}
+	}
+
 	@Override
 	public void onCreate() {
+		initReceiver();
+		initMediaPlayer();
+		initNotification();
+	}
+
+	private void initReceiver() {
+		receiver = new PhoneComingReceiver();
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction("android.intent.action.PHONE_STATE");
+		intentFilter.addAction("android.intent.action.NEW_OUTGOING_CALL");
+		registerReceiver(receiver, intentFilter);
+	}
+
+	private void initMediaPlayer() {
 		mPlayer = new MediaPlayer();
-		File directory  = new File(Environment.getExternalStorageDirectory(),"music");
-		System.out.println(directory.getAbsolutePath());
+		File directory = new File(Environment.getExternalStorageDirectory(), "music");
 		try {
 			musicList = FileUtil.getMusicsFromDir(directory);
+			mPlayer.reset();
 			mPlayer.setDataSource(musicList.get(musicIndex).localPath);
 			mPlayer.prepare();
 			mPlayer.setOnCompletionListener(new CompletionListener());
@@ -37,21 +86,30 @@ public class PlayerService extends Service {
 			e.printStackTrace();
 			Intent intent = new Intent();
 			intent.setAction(PlayerActivity.UPDATE_UI);
-			intent.putExtra(ERROR, true );
+			intent.putExtra(ERROR, true);
 			sendBroadcast(intent);
 			stopSelf(1);
 		}
 	}
-
-	private class CompletionListener implements OnCompletionListener{
-		@Override
-		public void onCompletion(MediaPlayer mp) {
-
-			next();
-		}
-		
+	
+	private void initNotification(){
+		notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		notification = new Notification(R.drawable.icon,getMusicName(),System.currentTimeMillis());
+		notification.flags = Notification.FLAG_INSISTENT;
+		Intent intent = new Intent(this,PlayerActivity.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+		notification.setLatestEventInfo(getApplicationContext(), "轻音乐播放器", getMusicName(), contentIntent);
+		notificationManager.notify(1, notification);
 	}
 	
+	private class CompletionListener implements OnCompletionListener {
+		@Override
+		public void onCompletion(MediaPlayer mp) {
+			next();
+		}
+
+	}
+
 	// 绑定时返回自身对象
 	public class PlayerBinder extends Binder {
 		PlayerService getService() {
@@ -67,55 +125,64 @@ public class PlayerService extends Service {
 	@Override
 	public boolean onUnbind(Intent intent) {
 		mPlayer.release();
+		notificationManager.cancel(1);
 		return super.onUnbind(intent);
 	}
-
 	
+	@Override
+	public void onDestroy() {
+		unregisterReceiver(receiver);
+		super.onDestroy();
+	}
+
 	public void pause() {
 		mPlayer.pause();
 	}
 
 	public void start() {
 		mPlayer.start();
+		Intent intent = new Intent();
+		intent.setAction(PlayerActivity.UPDATE_UI);
+		sendBroadcast(intent);
 	}
-	
-	//得到总时长
-	public String getDuration(){
+
+	// 得到总时长
+	public String getDuration() {
 		int duration = mPlayer.getDuration();
-		int minute = duration/1000/60;
-	    int second = duration/1000%60;
-	    return appendZero(minute).concat(appendZero(second));
+		int minute = duration / 1000 / 60;
+		int second = duration / 1000 % 60;
+		return appendZero(minute).concat(appendZero(second));
 	}
-	
-	//得到当前播放位置
-	public String getCurrentPosition(){
+
+	// 得到当前播放位置
+	public String getCurrentPosition() {
 		int currentPosition = mPlayer.getCurrentPosition();
-		int minute = currentPosition/1000/60;
-	    int second = currentPosition/1000%60;
-	    return appendZero(minute).concat(appendZero(second));
+		int minute = currentPosition / 1000 / 60;
+		int second = currentPosition / 1000 % 60;
+		return appendZero(minute).concat(appendZero(second));
 	}
-	
-	public String getMusicName(){
+
+	public String getMusicName() {
 		return musicList.get(musicIndex).musicName;
 	}
-	
-	public String getArtist(){
+
+	public String getArtist() {
 		return musicList.get(musicIndex).artist;
 	}
-	
-	//自动补零函数
-	private String appendZero(int num){
-		if(num<10)
+
+	// 自动补零函数
+	private String appendZero(int num) {
+		if (num < 10)
 			return String.valueOf(num).concat("0");
 		else
 			return String.valueOf(num);
 	}
-	
+
 	public void next() {
 		Intent intent = new Intent();
 		intent.setAction(PlayerActivity.UPDATE_UI);
 		sendBroadcast(intent);
-		if(musicIndex == musicList.size()-1){
+		if (musicIndex == musicList.size() - 1) {
 			musicIndex = -1;
 		}
 		musicIndex++;
@@ -126,11 +193,11 @@ public class PlayerService extends Service {
 			mPlayer.start();
 		} catch (Exception e) {
 			intent.setAction(PlayerActivity.UPDATE_UI);
-			intent.putExtra(ERROR, true );
+			intent.putExtra(ERROR, true);
 			sendBroadcast(intent);
 			stopSelf(1);
 			e.printStackTrace();
 		}
 	}
-	
+
 }
